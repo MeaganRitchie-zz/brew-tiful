ddf8aa1a9f3e67cac251240b5bff0f0520cb1441
Anika Bernstein <anikajb92@gmail.com>
2021-04-19 17:53:47 -0600

    Functionality of 'add to cart' button, added cart component

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
 export default function Card(props) {
+
+  const handleClick = () => {
+    props.clickAction(props.item)
+  }
+
   return (
     <div>
-      <img src={props.item.image} alt='food' />
+      <img src={ props.item.image } alt='food' />
+      <h1>{ props.item.name }</h1>
+      <button onClick={ handleClick }>Add To Cart</button>
     </div>
   )
 }
--- src/MenuContainer.js
+++ src/MenuContainer.js
@@ -1,16 +1,18 @@
-import React from 'react'
-import Card from './Card'
+import React from 'react';
+import Card from './Card';
+
 export default function MenuContainer(props) {
 
   const renderCard = () => {
     return props.menu.map(item => {
-      return <Card item={item} />
+      return <Card item={ item } key={ item.name } clickAction={ props.clickAction }/>
     })
   }
 
 
   return (
     <div>
+      <h1>Menu Items</h1>
       {renderCard()}
     </div>
   )
