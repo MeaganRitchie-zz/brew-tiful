f21d9b22802578cc85a52ceab458047fdc9372c0
Anika Bernstein <anikajb92@gmail.com>
2021-04-20 12:15:40 -0600

    Changed some menu names, changed alignments

--- src/Card.js
+++ src/Card.js
@@ -9,8 +9,10 @@ export default function Card(props) {
   return (
     <div className="menu-item">
       <img src={ props.item.image } alt='food' />
-      <h1>{ props.item.name }</h1>
-      <button onClick={ handleClick } className="button" >Add To Cart</button>
+      <div className="menu-item-details">
+        <h1>{ props.item.name }</h1>
+        <button onClick={ handleClick } className="button" >Add To Cart</button>
+      </div>
     </div>
   )
 }
