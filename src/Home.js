6f1479ae144d7da98d75328c5fa2631c7a13f666
Anika Bernstein <anikajb92@gmail.com>
2021-04-21 11:02:10 -0600

    Added thumbnails and some styling to home page, not complete

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import './Home.css';
+import Macaron_Stack from './backgrounds/macaron_stack.png';
+import Rasp_Macaron from './backgrounds/rasp_macaron.png';
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+  <section> 
+      <div className="circle"></div>
+      <div className="content">
+        <div className="textBox">
+          <h2>It's not just coffee 
+            <br/> 
+            It's <span>Brew-tiful</span>
+          </h2>
+          <p>Really good and yummy puns here about food and goodies. 
+            Really good and yummy puns here about food and goodies. 
+            Really good and yummy puns here about food and goodies. 
+            Really good and yummy puns here about food and goodies. 
+            Really good and yummy puns here about food and goodies. 
+            Really good and yummy puns here about food and goodies. 
+          </p>
+          <a href="./menu">See Our Menu</a>
+        </div>
+        <div className="imgBox">
+          <img src={Macaron_Stack} class="brewtiful"></img>
+        </div>
+        < ul class="thumb">
+          <li><img src={Rasp_Macaron}></img></li>
+          <li><img src=""></img></li>
+          <li><img src=""></img></li>
+        </ul>
+      </div>
+    </section>
   )
 }
